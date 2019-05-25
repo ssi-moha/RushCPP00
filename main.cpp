25574c49147f02b882bb0bcbbdd372f4b5a83746
Unknown <kil94@live.fr>
2019-05-25 17:22:40 +0200

    Auto stash before rebase of "origin/master"

--- main.cpp
+++ main.cpp
@@ -2,21 +2,24 @@
 #include <ncurses.h>
 #include "Model/Enemy.hpp"
 #include "Model/Render.hpp"
+#include "Model/Player.hpp"
+#include "Controller/Game.hpp"
 
 int main()
 {	
+    srand(time(NULL));
 	initscr();
-	
+
     Enemy a(5, 8);
     Enemy b(7, 9);
     Render render;
 
     render.display(a);
     render.display(b);
-
-	getch();
-	endwin();
-
+	
     Player player("roger");
-    Game game(player);    
+    Game game(player);
+    game.run();
+    
+    endwin();
 }
